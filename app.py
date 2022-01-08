196e5f05b0903bc746752652ae70adb4d3a233a1
achechet <achechet@gmail.com>
2022-01-08 17:58:38 +0300

    none

--- app.py
+++ app.py
@@ -1,65 +1,70 @@
-import os
-from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import Flask, render_template, url_for, request
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-load_dotenv()
-PORT = os.getenv('PORT')
-HOST = os.getenv('HOST')
-
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'thisisasecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
-class User(db.Model):
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
     psw = db.Column(db.String(500), nullable=True)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
+    pr = db.relationship('Profiles', backref='users', uselist=False)
+
     def __repr__(self):
         return f"<users {self.id}>"
 
+
 class Profiles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), nullable=True)
     old = db.Column(db.Integer)
     city = db.Column(db.String(100))
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
         return f"<profiles {self.id}>"
 
-@app.route('/')
-def index():
-    print( url_for('index'))
-    return render_template("index.html", title="Главная")
 
-@app.route('/register', methods=("POST", "GET"))
-# @login_required
+@app.route("/")
+def index():
+    info = []
+    try:
+        info = Users.query.all()
+    except:
+        print("Ошибка чтения из БД")
+
+    return render_template("index.html", title="Главная", list=info)
+
+
+@app.route("/register", methods=("POST", "GET"))
 def register():
     if request.method == "POST":
         try:
             hash = generate_password_hash(request.form['psw'])
-            u = User(email=request.form['email'], psw=hash)
+            u = Users(email=request.form['email'], psw=hash)
             db.session.add(u)
             db.session.flush()
+
             p = Profiles(name=request.form['name'], old=request.form['old'],
-            city=request.form['city'], user_id = u.id)
+                         city=request.form['city'], user_id = u.id)
             db.session.add(p)
             db.session.commit()
         except:
             db.session.rollback()
             print("Ошибка добавления в БД")
 
+        return redirect(url_for('index'))
+
     return render_template("register.html", title="Регистрация")
 
-if __name__ == '__main__':
-    app.run(host=HOST, port=PORT, debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
